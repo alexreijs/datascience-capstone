0125b5593f24c92612bd650cddd711b597e8ef94
Sjakil <alex.reijs@gmail.com>
2016-03-28 19:46:33 +0200

    Alot of work on model

--- functions.R
+++ functions.R
@@ -1,4 +1,10 @@
+setwd("D:/Development/Repos/datasciencecoursera/Capstone project")
+
 loadLines <- function(location, randomSampleSize = 0) {
+  
+  #options(encoding = "UTF-8")
+  #options(encoding = "latin1")
+  
   
   # Create a connection to file and read lines
   con <- file(location, "r", blocking = FALSE)
@@ -20,13 +26,134 @@ loadLines <- function(location, randomSampleSize = 0) {
   
 }
 
+mergeSumDataTables <- function(dataTable1, dataTable2) {
+  dataTable <- merge(dataTable1, dataTable2, all.x = T, all.y = T, by = "tokens")
+  dataTable$frequency <- rowSums(dataTable[, .(frequency.x, frequency.y)], na.rm = T)
+  dataTable[, .(tokens, frequency)]
+}
+
+
+sliceToken <- function(token) {
+  tokens <- unlist(strsplit(token, "\\s"))
+  c(paste(tokens[1:(length(tokens)-1)], collapse = " "), tokens[length(tokens)])
+}
+
+
+sliceTail <- function(tokenString) {
+  gsub(".+\\s+", "", tokenString)
+}
+
+sliceHead <- function(tokenString) {
+  substring(tokenString, 1, (nchar(tokenString) - nchar(sliceTail(tokenString)) - 1))
+}
+
+
+
+doPreprocessing <- function(lines, maxGram = 4, chunkSize = 50000) {
+  nrChunks <- ceiling(length(lines) / chunkSize)
+  nGramDataTables <- list(NULL)  
+  
+  for (x in 1:nrChunks) {
+    ptm <- proc.time()
+    print(paste("Preprocessing chunk number:", x, "of", nrChunks))
+    corpus <- createCorpus(lines[((x-1) * chunkSize):(x*chunkSize)])
+    
+    for (n in 1:maxGram) {
+      print(paste("Creating n-Gram", n, "..."))
+      nGramDataTable <- createQuantedaNGramDataTable(corpus, n)
+      
+      if (x == 1)
+        nGramDataTables[[n]] <- nGramDataTable
+      else {
+        print(paste("Merging n-Gram of size:", nrow(nGramDataTables[[n]]), "..."))
+        nGramDataTables[[n]] <- mergeSumDataTables(nGramDataTables[[n]], nGramDataTable)
+      }
+    }
+    
+    print(proc.time() - ptm)
+  }
+  
+  for (n in 1:maxGram) {
+    ptm <- proc.time()
+    if (n > 1) {
+      print(paste("Getting head and tail", n, "..."))
+      #nGramDataTables[[n]] <- nGramDataTables[[n]][frequency > 1, ]
+      
+      slices <- mapply(sliceToken, nGramDataTables[[n]]$tokens)
+      heads <- slices[1, ]
+      tails <- slices[2, ]
+      names(heads) <- c()
+      names(tails) <- c()
+      
+      nGramDataTables[[n]]$head <- heads
+      nGramDataTables[[n]]$tail <- tails
+      setkeyv(nGramDataTables[[n]], c("tokens", "head"))
+    }
+    print(proc.time() - ptm)
+  }
+  
+  print("Gererating probabilities ...")
+  probabilities <- createProbabilities(nGramDataTables)
+  probabilities
+  
+}
+
+
 createCorpus <- function(lines) {
+  
+  lines <- toLower(lines)
+  
+  Encoding(lines) <- "latin1"
+  lines <- iconv(lines, "latin1", "ASCII", sub = "")
+  
+  lines <- gsub("([0-9:. -]{2,3})?[0-9]{1,2} ?(a|p)( |[.])?m[.]?", "", lines)
+  lines <- gsub("[0-9]{1,} ?(g|kg|lbs|cc|sq|ft)", "", lines)
+
+  corpus(lines)
+  
+}
+
+createCorpus__ <- function(lines) {
+  
+  corpus <- Corpus(VectorSource(lines))
+  
+  specialChars <- content_transformer(function(x, pattern, replacement) { gsub(pattern, replacement, x, perl = T) })
+  
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u0153", "'")  # convert â€œ
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u02DC", "'")  # convert â€™
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u2122", "'")  # convert â€˜
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u201D", "–")  # convert â€”
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u201C", "—")  # convert â€“
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u00A2", "-")  # convert â€¢
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC\u00A6", "…")  # convert â€¦
+  corpus <- tm_map(corpus, specialChars, "\u00E2\u20AC", "'")  # convert â€
+  
+  corpus <- tm_map(corpus, stripWhitespace)
+  corpus <- tm_map(corpus, content_transformer(tolower))
+  corpus <- tm_map(corpus, removeWords, readLines("./profanity.txt")) # http://fffff.at/googles-official-list-of-bad-words/
+  #corpus <- tm_map(corpus, removeWords, stopwords("english"))
+  #corpus <- tm_map(corpus, stemDocument)
+  
+  #corpus <- tm_map(corpus, specialChars, "([0-9:. -]{2,3})?[0-9]{1,2} ?(a|p)( |[.])?m[.]?", "")  # remove numbers along with am / pm notation
+  #corpus <- tm_map(corpus, specialChars, "[0-9]{1,} ?(g|kg|lbs|cc|sq|ft)", "")  # remove numbers along with common metrics
+  
+  corpus <- tm_map(corpus, removePunctuation)
+  corpus <- tm_map(corpus, removeNumbers)
+  
+  
+  corpus
+}
+
+createCorpus_ <- function(lines) {
   
   # Create corpus from lines vector
   corpus <- Corpus(VectorSource(lines))
   
   # Apply cleaning of corpus
   corpus <- tm_map(corpus, content_transformer(tolower))
+  
+  #corpus <- tm_map(corpus, stemDocument)
+
   
   # Create function to handle replacement using regular expressions
   specialChars <- content_transformer(function(x, pattern, replacement) { gsub(pattern, replacement, x, perl = T) })
@@ -55,19 +182,19 @@ createCorpus <- function(lines) {
   #corpus <- tm_map(corpus, removeWords, readLines("./minimal-stop.txt"))   # http://www.text-analytics101.com/2014/10/all-about-stop-words-for-text-mining.html
   #corpus <- tm_map(corpus, removeWords, readLines("./terrier-stop.txt"))   # http://www.text-analytics101.com/2014/10/all-about-stop-words-for-text-mining.html
 
-  # Remove punctuation and special characters
-  #corpus <- tm_map(corpus, removePunctuation)
-  corpus <- tm_map(corpus, specialChars, "[\\\"'¦“”$*&+<>]", "")  # remove some characters
-  corpus <- tm_map(corpus, specialChars, "\\/", "")  # remove some characters
-  corpus <- tm_map(corpus, specialChars, "\\\\", "")  # remove some characters
-  corpus <- tm_map(corpus, specialChars, "\\-", "")  # remove some characters
-  
   
   # Remove numbers and any common metrics or time indication
   corpus <- tm_map(corpus, specialChars, "([0-9:. -]{2,3})?[0-9]{1,2} ?(a|p)( |[.])?m[.]?", "")  # remove numbers along with am / pm notation
   corpus <- tm_map(corpus, specialChars, "[0-9]{1,} ?(g|kg|lbs|cc|sq|ft)", "")  # remove numbers along with common metrics
   corpus <- tm_map(corpus, removeNumbers) 
 
+  # Remove punctuation and special characters
+  corpus <- tm_map(corpus, removePunctuation)
+  #corpus <- tm_map(corpus, specialChars, "[\\\"'¦“”$*&+<>]", "")  # remove some characters
+  #corpus <- tm_map(corpus, specialChars, "\\/", "")  # remove some characters
+  #corpus <- tm_map(corpus, specialChars, "\\\\", "")  # remove some characters
+  #corpus <- tm_map(corpus, specialChars, "\\-", "")  # remove some characters
+  
   # Remove excess whitespace
   corpus <- tm_map(corpus, stripWhitespace)
   
@@ -81,36 +208,66 @@ createCorpus <- function(lines) {
   
 }
 
-createDtmNgram <- function (corpus, n) {
+
+
+createQuantedaNGramDataTable <- function (corpus, n) {
   
-  # Create n-gram tokenizer function
-  tokenizer <- function(x) NGramTokenizer(x, Weka_control(min = n, max = n))
+  tokens <- tokenize(corpus,
+                     concatenator = " ",
+                     #ngram = n,
+                     verbose = FALSE,
+                     removeNumbers = TRUE,
+                     removeHyphens = TRUE,
+                     removePunct = TRUE,
+                     removeTwitter = TRUE)
   
-  # Create Term Document Matrix using tokenizer
-  dtm <- DocumentTermMatrix(corpus, control = list(tokenize = tokenizer))
+  #tokens <- removeFeatures(tokens, stopwords("english"))
+  #tokens <- removeFeatures(tokens, readLines("./profanity.txt"))
   
-  # Return Term Document Matrix
-  dtm  
+  tokens <- ngrams(tokens, n = n, concatenator = " ")
+  
+  dfm <- dfm(tokens, verbose = FALSE)
+  dfreq <- docfreq(dfm)
+  
+  dataTable <- data.table(tokens = names(dfreq), frequency = dfreq)
+  setkey(dataTable, tokens)
+  dataTable
   
 }
 
 
-createWekaNGramDataFrame <- function(corpus, n) {
+
+
+createProbabilities <- function(nGramDataTables) {
   
-  # Convert corpus into readable data to RWeka tokenizer
-  dataFrame <- data.frame(text = unlist(sapply(corpus, '[',"content")), stringsAsFactors = F)
+  probabilityDataTables <- list(NULL)
   
-  # Create n-gram tokenizer function
-  tokens <- NGramTokenizer(dataFrame, Weka_control(min = n, max = n))
+  for (n in 1:(length(nGramDataTables)-1)) {
+ 
+    nGramDataTable <- nGramDataTables[[n]]
+    nGramDataTableNext <- nGramDataTables[[n + 1]]
+    
+    dataTable <- suppressWarnings(merge(nGramDataTable, nGramDataTableNext, all.x = F, by.x = "tokens", by.y = "head"))
+    dataTable$probability <- dataTable$frequency.y / dataTable$frequency.x
+    
+    if (n == 1)
+      colnames(dataTable) <- c("tokens", "a", "b", "c", "nextWord", "probability")
+    else 
+      colnames(dataTable) <- c("tokens", "a", "b", "c", "e", "f", "nextWord", "probability")
+    
+    dataTable <- dataTable[, .(tokens, nextWord, probability)]
+    
+    probabilityDataTables[[n]] <- dataTable
+    
+    setkey(dataTable, tokens)
+    
+  }
   
-  # Prepare data frame from tokens
-  dataFrame <- data.frame(table(tokens))
-  dataFrame <- dataFrame[order(dataFrame$Freq, decreasing = TRUE), ]  
-  
-  # Return data frame
-  dataFrame
-  
+  probabilityDataTables
+
 }
+
+
 
 createWordCloud <- function(dataFrame, n = 10, scale = c(3, 1)) {
   
@@ -139,3 +296,44 @@ wordsNeeded <- function(goal = 0.6, data) {
   wordCount 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+createWekaNGramDataTable <- function(corpus, n) {
+  
+  # Convert corpus into readable data to RWeka tokenizer
+  dataTable <- data.table(text = unlist(sapply(corpus, '[',"content")), stringsAsFactors = F)
+  
+  # Create n-gram tokenizer function
+  tokens <- NGramTokenizer(dataTable, Weka_control(min = n, max = n))
+  
+  # Prepare data frame from tokens
+  dataTable <- data.table(table(tokens))
+  
+  #dataTable <- dataTable[order(dataTable$N, decreasing = TRUE), ]  
+  
+  colnames(dataTable) <- c("tokens", "frequency")
+  
+  # Return data frame
+  dataTable
+  
+}
+
---
+++ model.R
@@ -0,0 +1,53 @@
+library(data.table)
+library(quanteda)
+#library(tm)
+
+source("functions.R")
+
+linesTwitter <- loadLines("./final/en_US/en_US.twitter.txt")
+linesBlogs <- loadLines("./final/en_US/en_US.blogs.txt")
+linesNews <- loadLines("./final/en_US/en_US.news.txt")
+lines <- c(linesTwitter, linesBlogs, linesNews)
+lines <- loadLines("./final/en_US/en_US.twitter.txt")
+
+
+tables <- doPreprocessing(lines, 4, 20000)
+
+
+
+
+#dataTable1 <- data.table(tokens = c("meh", "kek"), frequency = c(1,2))
+#dataTable2 <- data.table(tokens = c("fds", "kek"), frequency = c(1,2))
+#dataTable <- merge(dataTable1, dataTable2, all.x = T, all.y = T, by = "tokens")
+
+probabilities[[1]]["happy"][order(-probability), ]
+probabilities[[2]]["case of"][order(-probability), ]
+probabilities[[3]]["struggling but the"][order(-probability), ]
+
+
+#Rprof("nGramDataFrame.out", memory.profiling=T)
+Rprof("nGramDataFrame.out")
+#probabilities <- createProbabilities(nGramDataTables)
+#nGramDataTables <- createNgramDataTables(maxN)
+c <- createNGramDataTable(corpus, 4)
+Rprof(NULL)
+#summaryRprof("nGramDataFrame.out", memory="both")
+summaryRprof("nGramDataFrame.out")
+
+
+
+#Rprof(tmp <- tempfile())
+#probabilities <- createProbabilities(nGramDataTables)
+#Rprof()
+#plotProfileCallGraph(readProfileData(tmp), score = "total")
+
+
+#x = profr(createProbabilities(nGramDataFrames))
+#ggplot(x)
+
+#l <- lineprof(probabilities <- createProbabilities(nGramDataFrames))
+
+
+#system.time(corpus <- createCorpus(lines)) / randomSize
+#system.time(nGramDataFrames <- createNgramDataFrames(maxN)) / randomSize
+#system.time(probabilities <- createProbabilities(nGramDataFrames)) / randomSize
